b76cef1269167ff2734f3500cb7c787acac503ec
MYoung <XD_Yang@outlook.comHJVDMSM>
2016-11-07 01:32:48 +0800

    UPDClient.h

--- MFCApplication3/UPDClient.h
+++ MFCApplication3/UPDClient.h
@@ -1,13 +1,16 @@
 #pragma once
-
+#include "MFCApplication3Dlg.h"
 // UPDClient 命令目标
-
+class CMFCApplication3Dlg;
 class UPDClient : public CAsyncSocket
 {
 public:
 	UPDClient();
 	virtual ~UPDClient();
 	virtual void OnReceive(int nErrorCode);
+	void SetParent(CMFCApplication3Dlg * pDlg);
+private:
+	CMFCApplication3Dlg* m_pDlg;//对话框类指针变量
 };
 
 
